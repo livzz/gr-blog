cd517ab325a0ae26a33f9d67f9cb287468f809aa
Alibh Deori <alibh619@gmail.com>
2018-03-26 17:57:27 +0530

    Contact Page Integration completed with firebase and form validation

--- public/js/contact.js
+++ public/js/contact.js
@@ -1,3 +1,13 @@
+var config = {
+  apiKey: "AIzaSyCvGB9LKTvtVn8LOx0j_d7anLz27eIVk90",
+  authDomain: "gr-blog-42a8e.firebaseapp.com",
+  databaseURL: "https://gr-blog-42a8e.firebaseio.com",
+  projectId: "gr-blog-42a8e",
+  storageBucket: "gr-blog-42a8e.appspot.com",
+  messagingSenderId: "51529782072"
+};
+firebase.initializeApp(config);
+
 new Vue({
   el: "#app",
   data: {
@@ -7,7 +17,8 @@ new Vue({
     message: ""
   },
   methods: {
-    send: function() {
+    send: function(e) {
+      e.preventDefault();
       if (
         this.name === "" ||
         this.email === "" ||
@@ -16,7 +27,16 @@ new Vue({
       ) {
         toastr.error("None of the fields can be empty!!");
       } else {
-        toastr.success("We will get in touch soon!!");
+        firebase
+          .database()
+          .ref("contacts")
+          .push({
+            name: this.name,
+            email: this.email,
+            subject: this.subject,
+            message: this.message
+          })
+          .then(toastr.success("We will get in touch soon!!"));
       }
     }
   }
