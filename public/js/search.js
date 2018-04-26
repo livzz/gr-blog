e2a543a64f9bd51c48b48e35acea0c02265af5ec
Alibh Deori <alibh619@gmail.com>
2018-04-26 18:21:02 +0530

    css for blog post and logo

--- public/js/search.js
+++ public/js/search.js
@@ -1,3 +1,14 @@
-$("#search").click(function() {
-  alert("Clicked!!");
+$(document).ready(function() {
+  $("#searchButton").click(function() {
+    alert("Clicked!!");
+    console.log("Clicked!!");
+  });
+  console.log("Loaded!!");
 });
+
+// new Vue({
+//   el: "#search",
+//   mounted() {
+//     console.log("Is Mounted!!");
+//   }
+// });
