b219e97283394c12c12b7096a810154ef2220912
Alibh Deori <alibh619@gmail.com>
2018-04-27 18:55:06 +0530

    css

--- public/js/search.js
+++ public/js/search.js
@@ -1,14 +1,13 @@
-$(document).ready(function() {
-  $("#searchButton").click(function() {
-    alert("Clicked!!");
-    console.log("Clicked!!");
-  });
-  console.log("Loaded!!");
+$("#searchButton").click(function() {
+  $(".search-box").addClass("search-box-viewable");
 });
 
-// new Vue({
-//   el: "#search",
-//   mounted() {
-//     console.log("Is Mounted!!");
-//   }
-// });
+$(document).click(function(event) {
+  //if you click on anything except the modal itself or the "open modal" link, close the modal
+  if (!$(event.target).closest("#searchButton,.search-box").length) {
+    $("body")
+      .find(".search-box")
+      .removeClass("search-box-viewable");
+    console.log("close");
+  }
+});
---
+++ public/js/share.js
@@ -0,0 +1,3 @@
+$("#share").jsSocials({
+  shares: ["twitter", "facebook", "googleplus", "linkedin", "whatsapp"]
+});
